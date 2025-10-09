393d0d636721a2b2878dcd6118ef90feaf8b9bf5
Gustavo Koyanagui <gustavo.koyanagui@fatec.sp.gov.br>
2025-10-09 13:23:01 -0300

    [MOD] Desabilitando servidor tomcat e spring web por enquanto.
    Signed-off-by: Gustavo Koyanagui <gustavo.koyanagui@fatec.sp.gov.br>

--- src/main/java/fateczl/CriptoGitClient/CriptoGitClientApplication.java
+++ src/main/java/fateczl/CriptoGitClient/CriptoGitClientApplication.java
@@ -2,18 +2,25 @@ package fateczl.CriptoGitClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import fateczl.CriptoGitClient.service.ConsoleService;
-import fateczl.CriptoGitClient.service.RepositorioService;
+
 
 @SpringBootApplication
 public class CriptoGitClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CriptoGitClientApplication.class, args);
+		System.out.println("Iniciando CriptoGit Client...");
+		
+		// Executa a aplicação sem inicializar o Spring Boot automaticamente
 		ConsoleService consoleService = new ConsoleService();
 		consoleService.run();
 		consoleService.close();
+		
+		// Opcional: Inicializar Spring Boot apenas se necessário
+		// ConfigurableApplicationContext context = SpringApplication.run(CriptoGitClientApplication.class, args);
+		// context.close();
+		
+		System.out.println("Aplicação encerrada.");
 	}
 
 }
